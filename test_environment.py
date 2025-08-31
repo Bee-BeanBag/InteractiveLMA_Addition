5e5a1ce0226dfd641b25c75e95c16568de066a6a
Bee Lamsma <blamsma@oswego.edu>
2025-08-31 19:42:08 -0400

    Update test_environment.py

--- test_environment.py
+++ test_environment.py
@@ -1,20 +1,5 @@
 import urllib.request
 import zipfile
-from pyxlma import coords
-from pyxlma.lmalib.flash.cluster import cluster_flashes
-import pyxlma.lmalib.flash.properties
-from pyxlma.lmalib.flash.properties import flash_stats, filter_flashes
-from pyxlma.lmalib.grid import  create_regular_grid, assign_regular_bins, events_to_grid
-from pyxlma.lmalib.io import read as lma_read
-from pyxlma.plot.interactive import InteractiveLMAPlot
-from pyxlma.plot.xlma_base_plot import FractionalSecondFormatter
-from pyxlma.plot.interactive import output as lma_plot_output
-from pyxlma.plot.xlma_base_plot import subplot_labels, inset_view, BlankPlot
-from pyxlma.plot.xlma_plot_feature import color_by_time, plot_points, setup_hist, plot_3d_grid, subset, plot_2d_network_points
-import pyxlma.plot.xlma_super_plot_feature
-import pyxlma.plot.xlma_super_base_plot
-import pyxlma.plot.radar as lmarad
-import InteractiveLMA_Addition.Thesis_Code_Functs as lmafuncts
 import pandas as pd
 import warnings
 warnings.filterwarnings("ignore")
